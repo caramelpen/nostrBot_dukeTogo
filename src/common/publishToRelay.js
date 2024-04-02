b615fe7530a2b668c78edebd5ed269d5ea206bd7
OT <>
2024-04-02 19:38:54 +0900

    logic modification

--- src/common/publishToRelay.js
+++ src/common/publishToRelay.js
@@ -23,29 +23,29 @@ const publishToRelay = async (relay, ev, isAutoReply = false, originallyPubKey =
         displayName = await userDisplayName(relay, originallyPubKey);
         console.log("get userDisplayName:" + displayName);
     }
-    // relay.publish(ev).then(() => {
-    //     console.log("publishToRelay:success!" + ":" + (replyingtoaPost.length > 0 ? "find:" + displayName + "_" + originallyPubKey + ":" + replyingtoaPost + "\n=> " :"") + ev.content);
-    //     // autoReply からやってきた
-    //     if(isAutoReply && replyingtoaPost.length > 0) {
-    //         // 最終更新日時を保存
-    //         updateLastReplyTime(originallyPubKey, currUnixtime());
-    //     }
-    // }).catch((err) => {
-    //                 console.error("publishToRelay:failed to send event." + ev.content + "-" + err);
-    //             })
-
-    try {
-        await relay.publish(ev);
-        //relay.publish(ev);
+    relay.publish(ev).then(() => {
         console.log("publishToRelay:success!" + ":" + (replyingtoaPost.length > 0 ? "find:" + displayName + "_" + originallyPubKey + ":" + replyingtoaPost + "\n=> " :"") + ev.content);
         // autoReply からやってきた
-        if (isAutoReply && replyingtoaPost.length > 0) {
+        if(isAutoReply && replyingtoaPost.length > 0) {
             // 最終更新日時を保存
             updateLastReplyTime(originallyPubKey, currUnixtime());
         }
-    } catch (err) {
+    }).catch((err) => {
         console.error("publishToRelay:failed to send event." + ev.content + "-" + err);
-    }
+    });
+
+    // try {
+    //     await relay.publish(ev);
+    //     //relay.publish(ev);
+    //     console.log("publishToRelay:success!" + ":" + (replyingtoaPost.length > 0 ? "find:" + displayName + "_" + originallyPubKey + ":" + replyingtoaPost + "\n=> " :"") + ev.content);
+    //     // autoReply からやってきた
+    //     if (isAutoReply && replyingtoaPost.length > 0) {
+    //         // 最終更新日時を保存
+    //         updateLastReplyTime(originallyPubKey, currUnixtime());
+    //     }
+    // } catch (err) {
+    //     console.error("publishToRelay:failed to send event." + ev.content + "-" + err);
+    // }
                 
 };
 
