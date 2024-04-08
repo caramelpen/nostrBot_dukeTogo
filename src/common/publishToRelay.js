47f6bf1427247187dbd7560a1c481848404bd3a9
OT <>
2024-04-09 07:13:16 +0900

    minor corrections

--- src/common/publishToRelay.js
+++ src/common/publishToRelay.js
@@ -25,14 +25,14 @@ const publishToRelay = async (relay, ev, isAutoReply = false, originallyPubKey =
         }
 
         await relay.publish(ev).then(() => {
-            console.log("publishToRelay:success!" + ":" + (replyingtoaPost.length > 0 ? "find:" + displayName + "_" + originallyPubKey + ":" + replyingtoaPost + "\n=> " :"") + ev.content);
+            console.log("publishToRelay:success!" + "\n" + (replyingtoaPost.length > 0 ? "find:" + displayName + ":" + originallyPubKey + ":" + replyingtoaPost + "\n=> " :"") + ev.content);
             // autoReply からやってきた
             if(isAutoReply && replyingtoaPost.length > 0) {
                 // 最終更新日時を保存
                 updateLastReplyTime(originallyPubKey, currUnixtime());
             }
         }).catch((err) => {
-            console.error("publishToRelay:failed to send event." + ev.content + "-" + err);
+            console.error("publishToRelay:failed to send event" + "\n" + ev.content + "-" + err);
         });
 
     } catch (err) {
