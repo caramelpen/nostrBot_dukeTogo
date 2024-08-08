96540009564ab159a718302a05119dc75565b4a4
OT <>
2024-08-08 20:01:00 +0900

    modified 'userDisplayName' again

--- src/common/publishToRelay.js
+++ src/common/publishToRelay.js
@@ -22,7 +22,8 @@ const publishToRelay = async (relay, ev, isAutoReply = false, originallyPubKey =
         let findChar = "";
         let displayName = "";
         if(originallyPubKey.length > 0){
-            displayName = await userDisplayName(relay, originallyPubKey);
+            //displayName = await userDisplayName(relay, originallyPubKey);
+            displayName = userDisplayName(relay, originallyPubKey);
             findChar = findChar + "find:" + displayName + "( " + originallyPubKey + " )";
         }
         if(replyingtoaPost.length > 0 ) {
